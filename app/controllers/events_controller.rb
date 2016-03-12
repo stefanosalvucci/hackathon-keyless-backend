36b8377d43d339c62295f879e8e3da0d40807a59
stefanosalvucci <stefano.salvucci@gmail.com>
2016-03-12 13:55:52 +0100

    add actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,5 @@
+#hackathon mode ON
+
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
@@ -20,6 +22,37 @@ class EventsController < ApplicationController
   # GET /events/1/edit
   def edit
   end
+
+  def status_door
+    render json: {status: Event.last.status}
+  end
+
+  def toggle_status
+    if Event.last.status == 'opened'
+      'closed'
+    else
+      'opened'
+    end
+  end
+
+  # POST /events
+  # POST /events.json
+  # same as create
+  def toggle_door
+    @event = Event.new(event_params)
+    @event.status = toggle_status
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   # POST /events
   # POST /events.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'events#index'
-  resources :events
+  resources :events do
+    collection do
+      get 'toggle_door'
+      get 'status_door'
+    end
+  end
 end
